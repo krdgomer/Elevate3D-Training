217c076b2bc398690f3a77f3bb4c4c4216ccc2f4
krdgomer <krdg.omercan@hotmail.com>
2025-04-28 17:26:42 +0300

    deepforest inference

--- src/deepforest/inference.py
+++ src/deepforest/inference.py
@@ -7,6 +7,7 @@ model = main.deepforest()
 # Load a pretrained tree detection model from Hugging Face
 model.load_model(model_name="weecology/deepforest-tree", revision="main")
 
-sample_image_path = get_data("D:/nyquil/dev/Bitirme-Projesi/Elevate3D/src/BATC/deepforest/image.png")
-boxes = model.predict_image(path=sample_image_path,return_plot=False) 
+sample_image_path = get_data("D:/dev/projects/Bitirme-Projesi/Elevate3D-Training/src/deepforest/input.png")
+boxes = model.predict_image(path=sample_image_path) 
 print(boxes)
+plot_results(boxes)
